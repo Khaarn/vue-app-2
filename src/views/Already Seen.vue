0152e04cdd87fc6f29e21e31381df0989470f79f
DESKTOP-GO1SN2Q\shrik <jeanmichelsoler44@gmail.com>
2021-03-09 09:24:16 +0100

    clear of the projects and changes in the about pages

---
+++ src/views/Already Seen.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="seen">
+    <h1>There you will see all the books that you have read</h1>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/books.png">
+    <HelloWorld msg="J-M Soler Project"/>
   </div>
 </template>
 
