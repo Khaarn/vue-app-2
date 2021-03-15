f9be312aff7df818673ccf0c08c789ebece05516
DESKTOP-GO1SN2Q\shrik <jeanmichelsoler44@gmail.com>
2021-03-15 08:12:15 +0100

    add all the components for the project

---
+++ docs/precache-manifest.b5ffa29d793b9639b543f292b20e987c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a665ec7dbfd6ca5c965c",
+    "url": "/vue-app-2/css/about.d0ff7cf7.css"
+  },
+  {
+    "revision": "61f318806e6ba1ebd4cb",
+    "url": "/vue-app-2/css/app.86c97585.css"
+  },
+  {
+    "revision": "336b50112037a6965f8a",
+    "url": "/vue-app-2/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "ff23f90d01a0d4523421874f8a3c5e4b",
+    "url": "/vue-app-2/img/books.ff23f90d.png"
+  },
+  {
+    "revision": "04915bf5c4b02e52dc7971c3cac899cb",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "a665ec7dbfd6ca5c965c",
+    "url": "/vue-app-2/js/about.47df112d.js"
+  },
+  {
+    "revision": "61f318806e6ba1ebd4cb",
+    "url": "/vue-app-2/js/app.5c1f4586.js"
+  },
+  {
+    "revision": "336b50112037a6965f8a",
+    "url": "/vue-app-2/js/chunk-vendors.4a331f84.js"
+  },
+  {
+    "revision": "db02522de917df00c5dab4c8b8858dbe",
+    "url": "/vue-app-2/json/anime.json"
+  },
+  {
+    "revision": "db02522de917df00c5dab4c8b8858dbe",
+    "url": "/vue-app-2/json/books.json"
+  },
+  {
+    "revision": "db02522de917df00c5dab4c8b8858dbe",
+    "url": "/vue-app-2/json/film.json"
+  },
+  {
+    "revision": "db02522de917df00c5dab4c8b8858dbe",
+    "url": "/vue-app-2/json/manga.json"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
